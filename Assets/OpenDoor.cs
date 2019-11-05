e9c2a7b2107fa3c818d4f8a5005899922feb90b7
Erik W. Bjønnes <erik.bjonnes@knowit.no>
2019-11-05 15:46:37 +0100

    Adding functionality to open/close doors when touching handles. Works once or twice, then stops. Need to determine what state it is in.

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] private Animator doorAnimationController;
+    private static readonly int DoClose = Animator.StringToHash("DoClose");
+    private static readonly int DoOpen = Animator.StringToHash("DoOpen");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.enabled)
+        {
+            var isOpen = doorAnimationController.GetCurrentAnimatorStateInfo(0).IsName("Open");
+            doorAnimationController.SetTrigger(isOpen ? DoClose : DoOpen);
+        }
+    }
+}
