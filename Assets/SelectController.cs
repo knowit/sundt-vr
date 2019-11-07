e02fba84f59e0160c8f367d9e7fe572519c485bd
Erik W. Bjønnes <erik.bjonnes@knowit.no>
2019-11-07 10:35:34 +0100

    Created PlayerController.prefab. It switches from OVRPlayerController to FPSController if game is running in the editor.

---
+++ Assets/SelectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectController : MonoBehaviour
+{
+    [SerializeField] private GameObject ovrPlayerController;
+    [SerializeField] private GameObject fpsController;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            ovrPlayerController.SetActive(false);
+            fpsController.SetActive(true);
+        }
+        else
+        {
+            ovrPlayerController.SetActive(true);
+            fpsController.SetActive(false);
+        }
+    }
+
+}
