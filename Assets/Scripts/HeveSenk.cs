00ef5416c387444c9abcffecb160493d1f998af3
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-12-07 13:53:06 +0100

    moved scripts and added lightgun and nerfgun assets

---
+++ Assets/Scripts/HeveSenk.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HeveSenk : MonoBehaviour
+{
+
+    public GameObject bottom;
+    public GameObject legs;
+    public GameObject top;
+
+    public bool up;
+    public bool down;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (legs != null)
+        {
+            Debug.Log("update: legs " + legs.transform.localScale);
+            Debug.Log("legs.position: " + legs.transform.position);
+        }
+        if (top != null)
+        {
+            Debug.Log("update: top " + top.transform.localScale);
+            Debug.Log("top.position: " + top.transform.position);
+        }
+
+        if (top != null && legs != null)
+        {
+            if (up || Input.GetKeyDown(KeyCode.U))
+            {
+                Debug.Log("scaling up");
+                legs.transform.localScale += new Vector3(0.0f, 0.2f, 0.0f);
+            }
+            else if (down || Input.GetKeyDown(KeyCode.D))
+            {
+                legs.transform.localScale -= new Vector3(0.0f, 0.2f, 0.0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                legs.transform.localScale = Vector3.one;
+            }
+            top.transform.localScale = Vector3.one;
+        }
+    }
+}
---
+++ Assets/Scripts/SelectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectController : MonoBehaviour
+{
+    [SerializeField] private GameObject ovrPlayerController;
+    [SerializeField] private GameObject fpsController;
+    
+    private void Awake()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            ovrPlayerController.SetActive(false);
+            fpsController.SetActive(true);
+        }
+        else
+        {
+            ovrPlayerController.SetActive(true);
+            fpsController.SetActive(false);
+        }
+    }
+
+}
