d814ac8e3a289967320709ed6652b17faf7808a4
Erik W. Bjønnes <erik.bjonnes@knowit.no>
2020-01-09 13:54:35 +0100

    Now navAgent will move towards player, wait a bit, then proceed to the meeting room for the presentation

--- Assets/Scripts/EscortPlayer.cs
+++ Assets/Scripts/EscortPlayer.cs
@@ -15,16 +15,18 @@ public class EscortPlayer : MonoBehaviour
     private NavMeshAgent _nma;
     private NavMeshPath _path;
     private float prevDistance = 0;
-    
+
+    private bool goToPlayer = true;
     
     private void Awake()
     {
         _path = new NavMeshPath();
         
         _nma = GetComponent<NavMeshAgent>();
-        _nma.SetDestination(targets[_targetIndex].transform.position);
         player = GameObject.FindWithTag("Player").transform;
     }
+    
+    
     
     private IEnumerator FindNewTarget()
     {
@@ -41,59 +43,62 @@ public class EscortPlayer : MonoBehaviour
         _nma.SetPath(_path);
         _nma.isStopped = false;
     }
-    
-    
+
+
     private void Update()
     {
         //note, does not handle player moving towards target room faster than agent if exceeding speedup ratio
-       
-        //possible states
-        var targetDistance = Vector3.Distance(transform.position, targets[_targetIndex].transform.position);
-        if ( Math.Abs(targetDistance) < 0.5f)
+        if (!goToPlayer)
         {
-            Debug.LogError($"We have arrived at our destination, {targets[_targetIndex].name}, distance {targetDistance} ");
-            //we have arrived
-            //arrived at target room, should we disable this script? Will need to ensure we aren't blocking the entry to the room.
-            _nma.isStopped = true;
-            return;
-        }
-        
-        var currDistance = Vector3.Distance(transform.position, player.transform.position);
-        Debug.Log($"{currDistance} from player tagged object");
-        if (currDistance <= maxPlayerDistance)
-        {
-            //ensure that we are moving, since player is within max distance
-            _nma.isStopped = false; 
-                
-            //moving towards room, player following
-            var diff = prevDistance - currDistance;
-            //change speed to match player
-            _nma.speed += diff*0.1f;
-            
-        }
-        else
-        {
-            //player outside range, check if visible
-            RaycastHit hit = new RaycastHit();
-            var avatarPosition = gameObject.transform.position + Vector3.up;
-            var canSeePlayer = Physics.Raycast(avatarPosition, (player.transform.position+Vector3.up) - avatarPosition, out hit);
-            if (canSeePlayer)
+            //possible states
+            var targetDistance = Vector3.Distance(transform.position, targets[_targetIndex].transform.position);
+            if (Math.Abs(targetDistance) < 0.5f)
             {
-                //will wait a bit to see if player follows
+                Debug.LogError(
+                    $"We have arrived at our destination, {targets[_targetIndex].name}, distance {targetDistance} ");
+                //we have arrived
+                //arrived at target room, should we disable this script? Will need to ensure we aren't blocking the entry to the room.
                 _nma.isStopped = true;
-                //can play optional sound bite here
+                return;
+            }
+
+            var currDistance = Vector3.Distance(transform.position, player.transform.position);
+            Debug.Log($"{currDistance} from player tagged object");
+            if (currDistance <= maxPlayerDistance)
+            {
+                //ensure that we are moving, since player is within max distance
+                _nma.isStopped = false;
+
+                //moving towards room, player following
+                var diff = prevDistance - currDistance;
+                //change speed to match player
+                _nma.speed += diff * 0.1f;
+
             }
             else
             {
-                Debug.LogWarning($"Can't see the player, setting it as new target");
-                //player has moved away, lets set the player as new target
-                _nma.SetDestination(player.transform.position);
+                //player outside range, check if visible
+                RaycastHit hit = new RaycastHit();
+                var avatarPosition = gameObject.transform.position + Vector3.up;
+                var canSeePlayer = Physics.Raycast(avatarPosition,
+                    (player.transform.position + Vector3.up) - avatarPosition, out hit);
+                if (canSeePlayer)
+                {
+                    //will wait a bit to see if player follows
+                    _nma.isStopped = true;
+                    //can play optional sound bite here
+                }
+                else
+                {
+                    Debug.LogWarning($"Can't see the player, setting it as new target");
+                    //player has moved away, lets set the player as new target
+                    _nma.SetDestination(player.transform.position);
+                }
+
             }
 
-        }
-
-        prevDistance = currDistance;
-        //moving towards room, player stopped follow
+            prevDistance = currDistance;
+            //moving towards room, player stopped follow
             //player stopped following
             //player is visible, if within certain range, wait. play sound. 
             //player is visible, outside given range, move towards player
@@ -112,13 +117,27 @@ public class EscortPlayer : MonoBehaviour
                 tempy.enabled = false;
             }
 
-            //ray cast towards player.
-            //slow down if distance to player is increasing.
-
-            //speed up to normal speed if players is closing
-
-            //if not seen, change path to find player, 
         }
-        
-
+        else
+        {
+            if (_nma.remainingDistance < 0.01f)
+            {
+                goToPlayer = false;
+                Debug.Log("waiting for new target to take effect");
+                StartCoroutine(FindNewTarget());
+                
+            }
+        }
     }
+
+    private void Start()
+    {
+        //move in the direction of the player, stop n units away from player 
+        var position = player.position;
+        var direction = Vector3.Normalize(position - transform.position);
+        var nearPlayerTarget = position - (3*direction);
+        nearPlayerTarget.y = transform.position.y;
+        _nma.SetDestination(nearPlayerTarget);
+        goToPlayer = true;
+    }
+}
