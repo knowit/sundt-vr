e3fff7b3c6468126b5fdcebec62edb589dd78de4
Erik W. Bjønnes <erik.bjonnes@knowit.no>
2020-01-09 11:01:39 +0100

    Now agent can navigate player to meeting room, and start presentation. Need to add agent moving towards player at the start, playing soundbite when in range then escorting player to target.

--- Assets/DoPresentation.cs
+++ Assets/DoPresentation.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Video;
 
 public class DoPresentation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string guideName = "NavAgent";
+    [SerializeField] private VideoPlayer presentation;
+    
+    
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag.Equals("Player"))
+        {
+            //not the one, no not the one
+        }
+        else
+        {
+            //this is the one
+            if (other.name.Equals(guideName))
+            {
+                presentation.Play();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
